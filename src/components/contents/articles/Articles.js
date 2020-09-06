1cc6e6749dc5d98677ee509e975a7e56dd8f1ad8
luigigutierrez23 <luigijose.gutierrez@gmail.com>
2020-09-06 20:34:50 -0300

    GET Request for Administrators

--- src/components/contents/articles/Articles.js
+++ src/components/contents/articles/Articles.js
@@ -8,10 +8,60 @@ import "datatables.net-responsive";
 export default function Articles() {
   const wallpaper1 = `${urlAPI}/mostrar-img-articulo/lorem-ipsum-1+3576.jpg`;
   /*------------------------------------
+  Creación de Data Set
+  --------------------------------------*/
+  const dataSet = [
+    [
+      1,
+      wallpaper1,
+      "lorem-ipsum-1",
+      "Lorem Ipsum 1.",
+      "lalalal",
+      '<h2>Lorem ipsum 1</h2><img src="http://localhost:4000/mostrar-img/3576.jpg" className="py-3 img-fluid"alt=""/><div className="text-muted">Lorem ipsum, dolor sit amet consectetur adipisicingelit. Libero quod hic culpa commodi. Repudiandaedebitis nisi ab! Numquam accusantium, dolores rerumnatus officia, repellendus dolorem nesciuntcorporis, dolore debitis esse.Illum modi deseruntfugit quidem saepe dolorum nam cum ipsam sequipariatur explicabo tempora, quis sint debitisreprehenderit delectus quos, possimus natus nisi aliquam id. Assumenda reiciendis soluta sequicorrupti.</div><br /><div className="text-muted">Voluptate ut, cum beatae natus eos quidem blanditiisnon temporibus est. Illum numquam rerum eummolestias quae, necessitatibus quis, illo, vitaeblanditiis consectetur veniam libero perspiciatisperferendis laborum qui asperiores.</div><br /> <div className="text-muted">Nulla, unde at hic distinctio quia id! Corporisaspernatur placeat recusandae, dicta in doloresdignissimos tempora explicabo dolore suscipit aquis, iusto nesciunt unde ab animi architecto aliasveniam modi.Vero nisi libero corporis quis voluptatequibusdam est sint! Molestias asperiores, verovoluptas illum ducimus repudiandae porro eos oditdolorum culpa at ex quod optio non iure, sint iste?Modi.</div>',
+      "dsa5d4ad4ada5dsa",
+    ],
+  ];
+  /*------------------------------------
   Ejecutamos Data table
   --------------------------------------*/
   $(document).ready(function () {
-    $(".table").DataTable();
+    $(".table").DataTable({
+      data: dataSet,
+      columns: [
+        { title: "#" },
+        {
+          title: "Portada",
+          render: function (data) {
+            return `<img style="width:320px" src="${data}">`;
+          },
+        },
+        { title: "url" },
+        { title: "Titulo" },
+        { title: "Intro" },
+        { title: "Contenido" },
+        {
+          title: "Acciones",
+          render: function (data) {
+            return `
+
+              <a href="#">
+              
+              <svg style="color:black; background:orange; border-radius: 100%; width:35px; line-height:35px; text-align: center; padding:10px;"
+              aria-hidden="true" focusable="false" data-prefix="fas" data-icon="pencil-alt" class="svg-inline--fa fa-pencil-alt fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M497.9 142.1l-46.1 46.1c-4.7 4.7-12.3 4.7-17 0l-111-111c-4.7-4.7-4.7-12.3 0-17l46.1-46.1c18.7-18.7 49.1-18.7 67.9 0l60.1 60.1c18.8 18.7 18.8 49.1 0 67.9zM284.2 99.8L21.6 362.4.4 483.9c-2.9 16.4 11.4 30.6 27.8 27.8l121.5-21.3 262.6-262.6c4.7-4.7 4.7-12.3 0-17l-111-111c-4.8-4.7-12.4-4.7-17.1 0zM124.1 339.9c-5.5-5.5-5.5-14.3 0-19.8l154-154c5.5-5.5 14.3-5.5 19.8 0s5.5 14.3 0 19.8l-154 154c-5.5 5.5-14.3 5.5-19.8 0zM88 424h48v36.3l-64.5 11.3-31.1-31.1L51.7 376H88v48z"></path></svg>
+
+              </a>
+
+              <a href="#">
+
+              <svg style="color:white; background:#c00; border-radius: 100%; width:35px; line-height:35px; text-align: center; padding:10px;"
+              aria-hidden="true" focusable="false" data-prefix="fas" data-icon="trash-alt" class="svg-inline--fa fa-trash-alt fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M32 464a48 48 0 0 0 48 48h288a48 48 0 0 0 48-48V128H32zm272-256a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zM432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16z"></path></svg>
+
+              </a>     
+              `;
+          },
+        },
+      ],
+    });
   });
 
   return (
@@ -40,88 +90,7 @@ export default function Articles() {
                   <table
                     className="table table-striped dt-responsive"
                     style={{ width: "100%" }}
-                  >
-                    <thead>
-                      <tr>
-                        <th>#</th>
-                        <th width="320px">Portada</th>
-                        <th>Titulo</th>
-                        <th>Intro</th>
-                        <th>Url</th>
-                        <th>Contenido</th>
-                        <th>Acciones</th>
-                      </tr>
-                    </thead>
-
-                    <tbody>
-                      <tr>
-                        <td>1</td>
-                        <td>
-                          <img src={wallpaper1} className="img-fluid" alt="" />
-                        </td>
-                        <td>Titulo</td>
-                        <td>Intro</td>
-                        <td>Url</td>
-                        <td>
-                          <h2>Lorem ipsum 1</h2>
-                          <img
-                            src="http://localhost:4000/mostrar-img/1219.jpg"
-                            className="py-3 img-fluid"
-                            alt=""
-                          />
-                          <div className="text-muted">
-                            Lorem ipsum, dolor sit amet consectetur adipisicing
-                            elit. Libero quod hic culpa commodi. Repudiandae
-                            debitis nisi ab! Numquam accusantium, dolores rerum
-                            natus officia, repellendus dolorem nesciunt
-                            corporis, dolore debitis esse.Illum modi deserunt
-                            fugit quidem saepe dolorum nam cum ipsam sequi
-                            pariatur explicabo tempora, quis sint debitis
-                            reprehenderit delectus quos, possimus natus nisi
-                            aliquam id. Assumenda reiciendis soluta sequi
-                            corrupti.
-                          </div>
-                          <br />
-                          <div className="text-muted">
-                            Voluptate ut, cum beatae natus eos quidem blanditiis
-                            non temporibus est. Illum numquam rerum eum
-                            molestias quae, necessitatibus quis, illo, vitae
-                            blanditiis consectetur veniam libero perspiciatis
-                            perferendis laborum qui asperiores.
-                          </div>
-                          <br />
-                          <div className="text-muted">
-                            Nulla, unde at hic distinctio quia id! Corporis
-                            aspernatur placeat recusandae, dicta in dolores
-                            dignissimos tempora explicabo dolore suscipit a
-                            quis, iusto nesciunt unde ab animi architecto alias
-                            veniam modi.Vero nisi libero corporis quis voluptate
-                            quibusdam est sint! Molestias asperiores, vero
-                            voluptas illum ducimus repudiandae porro eos odit
-                            dolorum culpa at ex quod optio non iure, sint iste?
-                            Modi.
-                          </div>
-                        </td>
-                        <td>
-                          <div className="btn-group">
-                            <button
-                              type="button"
-                              className="btn btn-warning rounded-circle mr-2"
-                            >
-                              <i className="nav-icon fas fa-pencil-alt"></i>
-                            </button>
-
-                            <button
-                              type="button"
-                              className="btn btn-danger rounded-circle"
-                            >
-                              <i className="nav-icon fas fa-trash"></i>
-                            </button>
-                          </div>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
+                  ></table>
                 </div>
               </div>
             </div>
--- src/components/contents/slide/Slide.js
+++ src/components/contents/slide/Slide.js
@@ -7,11 +7,50 @@ import "datatables.net-responsive";
 
 export default function Slides() {
   const slide1 = `${urlAPI}/mostrar-img/1219.jpg`;
+
+  /*------------------------------------
+  Creación de Data Set
+  --------------------------------------*/
+  const dataSet = [[1, slide1, "Lorem Ipsum", "Lorem", "dsa5d4ad4ada5dsa"]];
+
   /*------------------------------------
   Ejecutamos Data table
   --------------------------------------*/
   $(document).ready(function () {
-    $(".table").DataTable();
+    $(".table").DataTable({
+      data: dataSet,
+      columns: [
+        { title: "#" },
+        {
+          title: "Imagen",
+          render: function (data) {
+            return ` <img style="width:320px;" src ="${data}">   `;
+          },
+        },
+        { title: "Descripcion" },
+        {
+          title: "Acciones",
+          render: function (data) {
+            return `
+
+              <a href="#">
+              
+              <svg style="color:black; background:orange; border-radius: 100%; width:35px; line-height:35px; text-align: center; padding:10px;"
+              aria-hidden="true" focusable="false" data-prefix="fas" data-icon="pencil-alt" class="svg-inline--fa fa-pencil-alt fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M497.9 142.1l-46.1 46.1c-4.7 4.7-12.3 4.7-17 0l-111-111c-4.7-4.7-4.7-12.3 0-17l46.1-46.1c18.7-18.7 49.1-18.7 67.9 0l60.1 60.1c18.8 18.7 18.8 49.1 0 67.9zM284.2 99.8L21.6 362.4.4 483.9c-2.9 16.4 11.4 30.6 27.8 27.8l121.5-21.3 262.6-262.6c4.7-4.7 4.7-12.3 0-17l-111-111c-4.8-4.7-12.4-4.7-17.1 0zM124.1 339.9c-5.5-5.5-5.5-14.3 0-19.8l154-154c5.5-5.5 14.3-5.5 19.8 0s5.5 14.3 0 19.8l-154 154c-5.5 5.5-14.3 5.5-19.8 0zM88 424h48v36.3l-64.5 11.3-31.1-31.1L51.7 376H88v48z"></path></svg>
+
+              </a>
+
+              <a href="#">
+
+              <svg style="color:white; background:#c00; border-radius: 100%; width:35px; line-height:35px; text-align: center; padding:10px;"
+              aria-hidden="true" focusable="false" data-prefix="fas" data-icon="trash-alt" class="svg-inline--fa fa-trash-alt fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M32 464a48 48 0 0 0 48 48h288a48 48 0 0 0 48-48V128H32zm272-256a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zM432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16z"></path></svg>
+
+              </a>     
+              `;
+          },
+        },
+      ],
+    });
   });
 
   return (
@@ -41,7 +80,7 @@ export default function Slides() {
                     className="table table-striped dt-responsive"
                     style={{ width: "100%" }}
                   >
-                    <thead>
+                    {/* <thead>
                       <tr>
                         <th>#</th>
                         <th width="320px">Imagen</th>
@@ -77,7 +116,7 @@ export default function Slides() {
                           </div>
                         </td>
                       </tr>
-                    </tbody>
+                    </tbody> */}
                   </table>
                 </div>
               </div>
